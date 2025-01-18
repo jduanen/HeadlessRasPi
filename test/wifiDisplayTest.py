07045918585e7c9129c34ec8164f049dc5c65bef
J. Duane Northcutt <duane.northcutt@gmail.com>
2025-01-17 18:07:27 -0800

    Started adding some more pages.

--- test/wifiDisplayTest.py
+++ test/wifiDisplayTest.py
@@ -7,8 +7,10 @@
 ################################################################################
 
 import sys
+import time
 import os
 
+#### move this to __init__.py
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 
@@ -23,15 +25,48 @@ class WiFiPage(InfoPage):
     def render(self):
         r = self.runCmd("/usr/bin/nmcli radio wifi")
         #### FIXME
-        print(f"RESULT: {r}")
-        #### self.font = ImageFont.truetype(<path/font.ttf>, 16)
-        self.draw.text((0, 0), r, font=self.font, fill=255)
+        if r == "disabled":
+            font = ImageFont.truetype("/usr/share/fonts/truetype/noto/NotoMono-Regular.ttf", 16)
+            s = "WiFi Disabled"
+            self.draw.text((0, 0), s, font=font, fill=255)
+        elif r == "enabled":
+            '''
+            wpa_cli status
+            iwgetid -m
+            if mode == Master:
+                iw dev wlan0 info
+                ?
+            elif mode == ?:
+                ?
+            else:
+                ?
+            '''
+            s = "WiFi Enabled"
+            self.draw.text((0, 0), s, font=self.font, fill=255)
+        else:
+            s = "Other"
+            self.draw.text((0, 0), s, font=self.font, fill=255)
 
-pageFuncs = (WiFiPage(), )
+class ConnectionPage(InfoPage):
+    def render(self):
+        self.draw.text((0, 0), "TBD", font=self.font, fill=255)
+
+class CpuPage(InfoPage):
+    def render(self):
+        r = self.runCmd("/usr/bin/uptime")
+        #### FIXME
+        vals = r.split(',')
+        #### self.font = ImageFont.truetype(<path/font.ttf>, 16)
+        self.draw.text((0, 0), vals[0].strip(), font=self.font, fill=255)
+        self.draw.text((0, 11), vals[2].strip(), font=self.font, fill=255)
+        self.draw.text((0, 22), vals[3].strip(), font=self.font, fill=255)
+        print(f"RESULT: {vals[0]}, {vals[2]}, {vals[3]}")
+        #### TODO add cpu temp and /proc/meminfo and df -k /
+
+pageFuncs = (WiFiPage(), ConnectionPage(), CpuPage())
 
 display = InfoDisplay(pageFuncs)
 
-display.displayPages(2)
+display.displayPages(DWELL_SECS)
 
-time.sleep(DWELL_SECS)
 display.clear()
