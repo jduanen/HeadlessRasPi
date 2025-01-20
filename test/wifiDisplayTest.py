2b24625db95865da4ea487e1a65c709c3536d263
J. Duane Northcutt <duane.northcutt@gmail.com>
2025-01-20 14:42:07 -0800

    Checkpoint.

--- test/wifiDisplayTest.py
+++ test/wifiDisplayTest.py
@@ -18,10 +18,21 @@ from lib.InfoPage import InfoPage
 from lib.InfoDisplay import InfoDisplay
 
 
-DWELL_SECS = 5
+DWELL_SECS = 15
 
 
 class WiFiPage(InfoPage):
+    @classmethod
+    def _parseOutput(self, s):
+        d = {}
+        for indx, line in enumerate(s.splitlines()):
+            if '=' in line:
+                k, v = line.split('=')
+                d[k] = v
+            else:
+                d[f"L{indx}"] = line
+        return d
+
     def render(self):
         r = self.runCmd("/usr/bin/nmcli radio wifi")
         #### FIXME
@@ -30,9 +41,32 @@ class WiFiPage(InfoPage):
             s = "WiFi Disabled"
             self.draw.text((0, 0), s, font=font, fill=255)
         elif r == "enabled":
+            r = self.runCmd("/usr/sbin/wpa_cli status")
+            info = WiFiPage._parseOutput(r)
             '''
-            wpa_cli status
-            iwgetid -m
+            lines = r.splitlines()
+            ifName = lines[0].split()[2].strip()
+            ifAddr = lines[1].split('=')[1]
+            freq = lines[2].split('=')[1]
+            ssid = lines[3].split('=')[1]
+            mode = lines[5].split('=')[1]
+            wpaState = lines[6].split('=')[1]
+            ipAddr = lines[7].split('=')[1]
+            '''
+            self.draw.text((0, 0), "WiFi Enabled", font=self.font, fill=255)
+            self.draw.text((72, 0), info[ifName[1:-1], font=self.font, fill=255)
+            self.draw.text((0, 11), f"{ssid};  {mode}", font=self.font, fill=255)
+            self.draw.text((0, 22), f"{ipAddr}; {wpaState}", font=self.font, fill=255)            
+            self.draw.text((0, 33), f"{ifAddr};  {freq}", font=self.font, fill=255)
+            if mode == "AP":
+                print("AP")
+                r = self.runCmd("/usr/sbin/iwgetid -m")
+                self.draw.text((0, 44), r, font=self.font, fill=255)
+            elif mode == "STA":
+                print("STA")
+            else:
+                print("UNKNOWN Mode")
+            '''
             if mode == Master:
                 iw dev wlan0 info
                 ?
@@ -41,11 +75,8 @@ class WiFiPage(InfoPage):
             else:
                 ?
             '''
-            s = "WiFi Enabled"
-            self.draw.text((0, 0), s, font=self.font, fill=255)
         else:
-            s = "Other"
-            self.draw.text((0, 0), s, font=self.font, fill=255)
+            self.draw.text((0, 0), "Radio State Unknown", font=self.font, fill=255)
 
 class ConnectionPage(InfoPage):
     def render(self):
