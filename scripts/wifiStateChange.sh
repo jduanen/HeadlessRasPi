bc8b9fdde1d8aab79adace41bc9ccfd1941d1113
J. Duane Northcutt <duane.northcutt@gmail.com>
2025-01-21 15:17:53 -0800

    Added call to systemDisplay.

--- scripts/wifiStateChange.sh
+++ scripts/wifiStateChange.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-
 #
-#sudo chmod +x /etc/NetworkManager/dispatcher.d/90wifi-state-change.sh
+# Present system info on the I2C-connected OLED display whenever the WiFi's state changes
+# 
+# move this and make it executable
+#   'sudo mv wifiStateChange.sh /etc/NetworkManager/dispatcher.d/90wifi-state-change.sh'
+#   'sudo chmod +x /etc/NetworkManager/dispatcher.d/90wifi-state-change.sh'
 #
 
-if [ "$2" = "up" ] || [ "$2" = "down" ]; then
-    # Your commands here
+export WORKON_HOME=/home/jdn/.virtualenvs
+source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
+workon WIFI
+
+if [ "$1" = "wlan0" ]; then
     echo "WiFi state changed to $2" >> /tmp/wifi_log.txt
-    # Add your custom commands or script calls here
+    #if [ "$2" = "up" ] || [ "$2" = "down" ]; then
+    /home/jdn/Code/HeadlessRasPi/src/systemDisplay.py
+    echo "DONE\n" >> /tmp/wifi_log.txt
 fi
