10e5a6efb56886e90575a2596d85d6f5518381c1
Duane Northcutt <duane.northcutt@gmail.com>
2025-01-24 11:02:31 -0800

    Changed path.

--- scripts/triggerDisplay.sh
+++ scripts/triggerDisplay.sh
@@ -13,6 +13,6 @@ export WORKON_HOME=/home/jdn/.virtualenvs
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 workon WIFI
 
-sudo python3 ${HOME}/Code/HeadlessRasPi/scripts/triggerDisplay.py
+sudo python3 /home/jdn/Code/HeadlessRasPi/scripts/triggerDisplay.py
 
 logger -t systemDisplay "triggerDisplay.py exited"
